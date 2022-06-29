d7c5b3f7e4efa83a4189e837322a3cf5482e86da
Mariam Sepoyan <68228432+Nayopes@users.noreply.github.com>
2022-06-29 17:07:42 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+    let wCount = 4;
+    let hCount = 4;
+    let bHeight = 100;
+    let bWidth = 100;
+    let mainDiv = document.getElementById('main')
+    for (let i = 0; i < wCount * hCount; i++) {
+        let a = createBox(bHeight, bWidth);
+        mainDiv.appendChild(a);
+        a.onmouseout = function () {
+            a.style.background = setRandomColor(this);
+        }
+    }
+
+    document.body.appendChild(mainDiv);
+
+
+document.querySelector(".ref").onclick = function () {
+    location.reload();
+}
+
+function createBox(h, w) {
+    let div = document.createElement("div");
+    div.style.height = h + "px";
+    div.style.width = w + "px";
+    div.style.border = "1px solid black";
+    div.style.boxSizing = "border-box";
+    div.style.background = "white";
+    div.innerHTML = "<img src='./images/lose.gif'>";
+
+    return div;
+}
+
+function setRandomColor(a) {
+    console.log(a);
+    return a.style.visibility = "hidden";
+}
